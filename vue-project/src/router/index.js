5b0b6de77cc5ee4602616ae07b28207d2eb41fa6
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-11-11 18:01:57 +0900

    24.11.11 : BaseLayout 작성

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,8 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import BaseLayout from '@/layout/BaseLayout.vue' // 레이아웃 컴포넌트
+
+const currentRoute = [
+  {
+    path: '/',
+    name: 'Base',
+    component: BaseLayout,
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: currentRoute,
 })
 
 export default router
