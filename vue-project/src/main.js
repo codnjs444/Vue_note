0d6fef6f4a5c406b031030bb003f51eed1e42ab2
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-11-12 15:28:58 +0900

    init

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,15 +1,15 @@
-import './assets/main.css'
-
+// main.js
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import vuetify from './plugins/vuetify/index' // 방금 만든 vuetify 설정 파일을 가져옵니다.
-
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
+
+// @ts-ignore
+import vuetify from './plugins/vuetify'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(vuetify) // Vuetify를 Vue 앱에 추가합니다.
+app.use(vuetify) // Vuetify를 Vue 앱에 추가
 app.mount('#app')
